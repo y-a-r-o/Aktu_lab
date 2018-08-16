93e43b361c3b7d17f88d71676334b525a956b9be
yashchandel0007 <yashchandel007@outlook.com>
2018-08-17 03:56:57 +0530

    Bottom Navigation selection not working properly!

--- app/src/main/java/com/example/lenovo_pc/aktu_lab/MainActivity.java
+++ app/src/main/java/com/example/lenovo_pc/aktu_lab/MainActivity.java
@@ -1,36 +1,56 @@
 package com.example.lenovo_pc.aktu_lab;
 
+import android.support.v4.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+      //  FragmentManager fragmentManager = getSupportFragmentManager();
+      //  FragmentTransaction transaction = fragmentManager.beginTransaction();
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
+            switch (item.getItemId()){
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                    setTitle("Home");
+                    HomeFragment fragment =new HomeFragment();
+                    FragmentTransaction fragmentTransaction= getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.mainframe,fragment,"Fragment");
+                    fragmentTransaction.commit();
+        //           transaction.replace(R.id.mainframe,new HomeFragment()).commit();
+                    break;
                 case R.id.navigation_search:
-
-                    return true;
+                    setTitle("Search");
+                    SearchFragment fragment2 =new SearchFragment();
+                    FragmentTransaction fragmentTransaction2= getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction2.replace(R.id.mainframe,fragment2,"Fragment");
+                    fragmentTransaction2.commit();
+                    // transaction.replace(R.id.mainframe,new Sear chFragment()).commit();
+                    break;
                 case R.id.navigation_myaccount:
-
-                    return true;
+                    setTitle("My Account");
+                    MyAccountFragment fragment3 =new MyAccountFragment();
+                    FragmentTransaction fragmentTransaction3= getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction3.replace(R.id.mainframe,fragment3,"Fragment");
+                    fragmentTransaction3.commit();
+          //          transaction.replace(R.id.mainframe,new MyAccountFragment()).commit();
+                    break;
             }
             return false;
         }
+
     };
 
     @Override
@@ -38,11 +58,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
+      //  FragmentManager fragmentManager = getSupportFragmentManager();
+      //  FragmentTransaction transaction=fragmentManager.beginTransaction();
+      //  transaction.replace(R.id.navigation,new HomeFragment()).commit();
+        setTitle("Home");
+        HomeFragment fragment =new HomeFragment();
+        FragmentTransaction fragmentTransaction= getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.mainframe,fragment,"Fragment");
+        fragmentTransaction.commit();
 
     }
 
