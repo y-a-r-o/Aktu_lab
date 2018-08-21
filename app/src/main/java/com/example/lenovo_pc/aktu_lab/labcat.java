750c077a2d2d3cca14ae887ae5d4c8774089f299
yashchandel0007 <yashchandel007@outlook.com>
2018-08-21 19:09:16 +0530

    Homefrag and labcat are working absolutely fine!

--- app/src/main/java/com/example/lenovo_pc/aktu_lab/labcat.java
+++ app/src/main/java/com/example/lenovo_pc/aktu_lab/labcat.java
@@ -11,19 +11,18 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SearchFragment extends Fragment {
+public class labcat extends Fragment {
 
 
-    public SearchFragment() {
+    public labcat() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        return inflater.inflate(R.layout.fragment_labcat, container, false);
     }
 
 }
