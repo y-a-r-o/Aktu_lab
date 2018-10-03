13ce0b438869c9b4b33f0c7d328c4bdd4517ed5e
Apoorv <aryan.kudesia@gmail.com>
2018-10-04 00:56:39 +0530

    Project change commit

---
+++ app/src/main/java/com/example/lenovo_pc/aktu_lab/imagewala.java
@@ -0,0 +1,31 @@
+package com.example.lenovo_pc.aktu_lab;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class imagewala extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imagewala);
+        }
+
+
+}
+
