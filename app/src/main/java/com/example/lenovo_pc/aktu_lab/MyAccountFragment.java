5cedeb27a0a35fc9b3adb173d29a49f736e074b8
Apoorv <aryan.kudesia@gmail.com>
2018-10-05 23:44:17 +0530

    Project change commit3

--- app/src/main/java/com/example/lenovo_pc/aktu_lab/MyAccountFragment.java
+++ app/src/main/java/com/example/lenovo_pc/aktu_lab/MyAccountFragment.java
@@ -7,12 +7,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.support.v7.widget.Toolbar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static com.firebase.ui.auth.AuthUI.getApplicationContext;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MyAccountFragment extends Fragment {
 
+    private FirebaseAuth auth;
+    Button b1;
 
     public MyAccountFragment() {
         // Required empty public constructor
@@ -23,7 +45,22 @@ public class MyAccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_account, container, false);
+        View view= inflater.inflate(R.layout.fragment_my_account, container, false);
+        b1=(Button)view.findViewById(R.id.button5);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth = FirebaseAuth.getInstance();
+                FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(v.getContext(), "" + currentFirebaseUser.getUid(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+        return view;
     }
+
 
 }
